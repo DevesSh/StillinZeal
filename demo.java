5a05298562a185f73d22f7129c65b0b31e2bf1f2
GodHead <71123960+DevesSh@users.noreply.github.com>
2022-02-13 01:11:22 +0530

    Delete demo.java

--- demo.java
+++
@@ -1,9 +0,0 @@
-Class StillinZeal
-{
-    public static void main(String args[])
-    {
-         System.out.println("Demo java rogram");
-         System.out.println("Hello World");
-         
-    }
-}
